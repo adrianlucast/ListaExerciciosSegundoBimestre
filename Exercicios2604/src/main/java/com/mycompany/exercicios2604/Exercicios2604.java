d394bed17152e307154c7c07c289faf9ccbe41be
adrianlucast <127361581+adrianlucast@users.noreply.github.com>
2023-04-26 20:07:38 -0300

    Add files via upload

---
+++ Exercicios2604/src/main/java/com/mycompany/exercicios2604/Exercicios2604.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exercicios2604;
+
+/**
+ *
+ * @author aluno
+ */
+public class Exercicios2604 {
+        
+        public static void main(String[] args) {
+        ingresso ingresso1 = new ingresso (75);
+        ingressoVIP ingressoVIP = new ingressoVIP (75, 35);
+        
+        System.out.println (ingressoVIP.toString());
+        
+    }
+}
---
+++ Exercicios2604/src/main/java/com/mycompany/exercicios2604/ingresso.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.exercicios2604;
+
+
+public class ingresso {
+    private float valor;
+    
+    public ingresso(float valor){
+        this.valor = valor;
+    }
+    
+    public void setValor(float valor){
+        this.valor = valor;
+    }
+    
+    public float getValor(){
+        return valor;
+    }
+}
+
